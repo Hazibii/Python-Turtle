38a55a1802e6c1262dcf6a4eb763043cbbb6860e
Hasib Sarvari <tkdparallax@gmail.com>
2016-09-23 14:40:45 +0800

    delete extra smile.py

--- smile.py
+++ smile.py
@@ -4,27 +4,25 @@ Python Turtles Tutorial
 http://hackerpals.com
 '''
 
+
 import turtle
 
 smiles = turtle.Turtle()
 smiles.penup()
 smiles.goto(-75,150)
 smiles.pendown()
-smiles.circle(10)     #eye one
+smiles.circle(10)     #Draw eye one
 
 smiles.penup()
 smiles.goto(75,150)
 smiles.pendown()
-smiles.circle(10)     #eye two
+smiles.circle(10)     #Draw eye two
 
 
 smiles.penup()
-smiles.goto(0,0)
+smiles.goto(-90,70)
 smiles.pendown()
-smiles.circle(100,90)   #right smile
+smiles.setheading(270) #set heading to go south
+smiles.circle(80,180)
 
-smiles.penup()
-smiles.setheading(180) # <-- look West
-smiles.goto(0,0)
-smiles.pendown()
-smiles.circle(-100,90)
+turtle.done() #Finish
